f41b69bad16fc8e3e7a61b763f8f8ef551fdf2c9
Chuck J hardy <chuckjhardy@gmail.com>
2014-01-11 13:36:00 +0000

    Add Basic Prototype Cell
    With a single label and an accessory of checkmark.

--- DevTests/OnTheBeach/OTBiOSDevTest/Tasks/OTBTasksViewController.m
+++ DevTests/OnTheBeach/OTBiOSDevTest/Tasks/OTBTasksViewController.m
@@ -17,13 +17,22 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 1;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Item"];
+    return cell;
 }
 
 @end
