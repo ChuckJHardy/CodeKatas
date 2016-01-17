114e9d93f6ad903bc4e0e64a797e95bea73175fb
Charles J Hardy <chuckjhardy@gmail.com>
2016-01-17 12:23:04 +0000

    Accept Page and PerPage in Posts Index

--- DevTests/NewsFeed/app/controllers/posts_controller.rb
+++ DevTests/NewsFeed/app/controllers/posts_controller.rb
@@ -1,7 +1,17 @@
 class PostsController < ApplicationController
   # GET /users/1/posts
   def index
-    @posts = Post.all
+    @posts = Post.page(page).per(per_page)
     render json: @posts
+  end
+
+  private
+
+  def page
+    params.fetch(:page, 1)
+  end
+
+  def per_page
+    params.fetch(:per_page, 10)
   end
 end
--- DevTests/NewsFeed/spec/requests/posts_spec.rb
+++ DevTests/NewsFeed/spec/requests/posts_spec.rb
@@ -1,14 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
-  let!(:post) { create(:post) }
+  let(:post) { create(:post) }
+
+  before do
+    create(:post)
+    post
+  end
 
   describe 'GET /users/1/posts' do
-    it 'returns Posts' do
-      get user_posts_path(post.user, format: :json)
+    before do
+      get user_posts_path(post.user, format: :json, per_page: 1, page: 2)
+    end
 
+    it 'response with expected status code' do
       expect(response.status).to eq(200)
+    end
+
+    it 'returns posts' do
       expect(response).to match_response_schema('posts')
+    end
+
+    it 'returns paginated results' do
+      expect(JSON.parse(response.body)['posts'].first['id']).to eq(post.id)
     end
   end
 end
